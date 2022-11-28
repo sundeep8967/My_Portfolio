37179d01079e4ff25fdf9a1b4b6198f186c88fb1
“sundeep8967” <sundeep8967@gmail.com>
2022-11-28 12:01:37 +0530

    education details edited

--- src/components/education/EducationData.jsx
+++ src/components/education/EducationData.jsx
@@ -1,12 +1,12 @@
 const EducationData = [
-    {
-      id: 1,
-      category: "education",
-      year: "School of Studies (2007 - 2017)",
-      title: "BPSC ( ICSE BOARD ) ",
-      desc: "Completed my primary level and secondary level education in this school",
-      rate: "83 %",
-    },
+    // {
+    //   id: 1,
+    //   category: "education",
+    //   year: "School of Studies (2007 - 2017)",
+    //   title: "BPSC ( ICSE BOARD ) ",
+    //   desc: "Completed my primary level and secondary level education in this school",
+    //   rate: "83 %",
+    // },
     // {
     //   id: 2,
     //   category: "education",
@@ -15,14 +15,14 @@ const EducationData = [
     //   desc: "Completed my first and second PUC in PCMB course in Alvas PU College",
     //   rate: "77 %",
     // },
-    // {
-    //   id: 3,
-    //   category: "education",
-    //   year: "Engineering (2019 - present) ",
-    //   title: "Ramaiah Institute Of Technology",
-    //   desc: "Currently pursuing my fourth Bachelor of Engineering in Computer Science and Engineering",
-    //   rate: "8.45 / 10 ",
-    // },
+    {
+      id: 3,
+      category: "education",
+      year: "Engineering (2019 - present) ",
+      title: "Ramaiah Institute Of Technology",
+      desc: "Currently pursuing my fourth Bachelor of Engineering in Computer Science and Engineering",
+      rate: "8.45 / 10 ",
+    },
  
   
   ]
