bb0eeb10ab5904dc888b7c2017a88bbb53a46f7f
“sundeep8967” <sundeep8967@gmail.com>
2022-11-28 11:59:24 +0530

    education details edited

--- src/components/education/EducationData.jsx
+++ src/components/education/EducationData.jsx
@@ -7,22 +7,22 @@ const EducationData = [
       desc: "Completed my primary level and secondary level education in this school",
       rate: "83 %",
     },
-    {
-      id: 2,
-      category: "education",
-      year: "College of Studies (2017 - 2019) ",
-    title: "Narayana ( STATE BOARD )",
-      desc: "Completed my first and second PUC in PCMB course in Alvas PU College",
-      rate: "77 %",
-    },
-    {
-      id: 3,
-      category: "education",
-      year: "Engineering (2019 - present) ",
-      title: "Ramaiah Institute Of Technology",
-      desc: "Currently pursuing my fourth Bachelor of Engineering in Computer Science and Engineering",
-      rate: "8.45 / 10 ",
-    },
+    // {
+    //   id: 2,
+    //   category: "education",
+    //   year: "College of Studies (2017 - 2019) ",
+    // title: "Narayana ( STATE BOARD )",
+    //   desc: "Completed my first and second PUC in PCMB course in Alvas PU College",
+    //   rate: "77 %",
+    // },
+    // {
+    //   id: 3,
+    //   category: "education",
+    //   year: "Engineering (2019 - present) ",
+    //   title: "Ramaiah Institute Of Technology",
+    //   desc: "Currently pursuing my fourth Bachelor of Engineering in Computer Science and Engineering",
+    //   rate: "8.45 / 10 ",
+    // },
  
   
   ]
