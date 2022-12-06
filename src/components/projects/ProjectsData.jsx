a8f5e195caec9ab67e984e41b1d0701808c48808
“sundeep8967” <sundeep8967@gmail.com>
2022-12-06 20:45:26 +0530

    .

--- src/components/projects/ProjectsData.jsx
+++ src/components/projects/ProjectsData.jsx
@@ -1,24 +1,24 @@
 const Projects_Data = [
-    {
-      id: 1,
-      category: "FRONTEND DEVELOPMENT",
-      totalLike: "600",
-      title: "My Portfolio Website",
-      image: "./assets/images/portfolio.png",
-      content:"This website is about myself, my eduaction, collection of my best development projects that accurately represent my skills, my abilities, and the quality of my work.",
-      gitlink: "https://github.com/manoj-kumar-m/travel-guide",
-      languages:['ReactJs','CSS','JS']
-    },
-    { 
-      id: 2,
-      category: "MERN STACK DEVELOPMENT",
-      totalLike: "750",
-      title: "Travel Guide Website",
-      image: "./assets/images/travelguide.png",
-      content:"A Web Application for planning trips or tours that takes the location(state, district, or city) and the number of days as input from the user and provides a map with all the places that can be visited in that location in the given period plotted and connected. It also recommends hotels and other amenities based on user reviews.",
-      gitlink  : "https://github.com/manoj-kumar-m/travel-guide",
-      languages:['ReactJs','CSS','NodeJs','ExpressJs','MongoDB']
-    },
+    // {
+    //   id: 1,
+    //   category: "FRONTEND DEVELOPMENT",
+    //   totalLike: "600",
+    //   title: "My Portfolio Website",
+    //   image: "./assets/images/portfolio.png",
+    //   content:"This website is about myself, my eduaction, collection of my best development projects that accurately represent my skills, my abilities, and the quality of my work.",
+    //   gitlink: "https://github.com/manoj-kumar-m/travel-guide",
+    //   languages:['ReactJs','CSS','JS']
+    // },
+    // { 
+    //   id: 2,
+    //   category: "MERN STACK DEVELOPMENT",
+    //   totalLike: "750",
+    //   title: "Travel Guide Website",
+    //   image: "./assets/images/travelguide.png",
+    //   content:"A Web Application for planning trips or tours that takes the location(state, district, or city) and the number of days as input from the user and provides a map with all the places that can be visited in that location in the given period plotted and connected. It also recommends hotels and other amenities based on user reviews.",
+    //   gitlink  : "https://github.com/manoj-kumar-m/travel-guide",
+    //   languages:['ReactJs','CSS','NodeJs','ExpressJs','MongoDB']
+    // },
     
     
    
